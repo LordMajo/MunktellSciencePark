9109c6dfc7d75fc1a3878b04110d58b249759fc0
Lord Majo <majo@tii.se>
2016-12-15 15:01:00 +0100

    Added mutt data
    blablabla

--- Assets/Scripts/Mqtt/Test/mqttTest.cs
+++ Assets/Scripts/Mqtt/Test/mqttTest.cs
@@ -5,15 +5,19 @@ using uPLibrary.Networking.M2Mqtt;
 using uPLibrary.Networking.M2Mqtt.Messages;
 using uPLibrary.Networking.M2Mqtt.Utility;
 using uPLibrary.Networking.M2Mqtt.Exceptions;
-
+using UnityEngine.UI;
 using System;
 
+
 public class mqttTest : MonoBehaviour {
+
 	private MqttClient client;
 
-    public String mspPowerTopic = "Testsites/MunktellSiencePark/mainmeter/meterevent";
+	public Text mspPowerText;
+    public String mspPowerTopic = "/msp/";
+	public String mqttMsg;
+
     // public String mqttServerAdress = "213.168.249.129";
-    public String testMessage = "helloen";
 
     // Use this for initialization
     void Start () {
@@ -27,26 +31,33 @@ public class mqttTest : MonoBehaviour {
 		client.Connect(clientId);
 
         // subscribe to the topic mspPowerTopic with QoS 2 
-        client.Subscribe(new string[] { "/msp/" }, new byte[] { MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE }); 
+		client.Subscribe(new string[] { "/msp/power/" }, new byte[] { MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE }); 
 
 	}
-	void client_MqttMsgPublishReceived(object sender, MqttMsgPublishEventArgs e) 
-	{ 
 
-		Debug.Log("Received: " + System.Text.Encoding.UTF8.GetString(e.Message)  );
+
+	void client_MqttMsgPublishReceived(object sender, MqttMsgPublishEventArgs e) { 
+
+		//Debug.Log("Received: " + System.Text.Encoding.UTF8.GetString(e.Message)  );
+		mqttMsg = System.Text.Encoding.UTF8.GetString (e.Message);
+		Debug.Log(mqttMsg);
+
 	} 
+
+	/*
 
 	void OnGUI(){
 		if ( GUI.Button (new Rect (20,40,80,20), "Level 1")) {
 			Debug.Log("sending...");
-			client.Publish("/msp/", System.Text.Encoding.UTF8.GetBytes("Sending from Unity3D!!!"), MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE, true);
+			client.Publish("/msp/chat/", System.Text.Encoding.UTF8.GetBytes("MSP Unity connected"), MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE, true);
 			Debug.Log("sent");
 		}
 	}
+	
+	*/
+
 	// Update is called once per frame
 	void Update () {
-
-
-
+		mspPowerText.text = mqttMsg;
 	}
 }
