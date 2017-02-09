00061ed9f971f2d5ee5b81e1416517c3f7409f96
Belking <johan@belking.se>
2017-02-09 12:46:05 +0100

    added stuff

---
+++ Assets/Art/movies/Play_movie.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Play_movie : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		((MovieTexture)GetComponent<Renderer>().material.mainTexture).Play();
+	}
+}
