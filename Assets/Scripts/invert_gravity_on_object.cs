e9e3a96d2e1da902b734404ae4836bd759f371cb
Belking <johan@belking.se>
2017-02-17 15:28:19 +0100

    news

---
+++ Assets/Scripts/invert_gravity_on_object.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class invert_gravity_on_object : MonoBehaviour {
+	
+	public float thrust;
+	public Rigidbody rb;
+	// Use this for initialization
+	void Start() 
+	{
+			
+		rb = GetComponent<Rigidbody>();
+		rb.useGravity = false;
+	}
+
+	void FixedUpdate() 
+	{
+		rb.AddForce(0, 15f, 0 * thrust);
+	}
+}
