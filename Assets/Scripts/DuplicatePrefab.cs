67f3702e1734f0afc13d5185d920a88f0f834da8
Lord Majo <majo@tii.se>
2017-02-20 11:38:51 +0100

    sf's
    sdfsdf

---
+++ Assets/Scripts/DuplicatePrefab.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class DuplicatePrefab : MonoBehaviour {
+
+	public GameObject prefab;
+	public int prefabCount;
+
+	public float spawnOffsetHigh;
+	public float spawnOffsetLow;
+	public float dropHeight;
+	private int i;
+
+
+
+	void Update() {
+		
+		if (i < prefabCount) {
+		
+			Instantiate(prefab, new Vector3(Random.Range(spawnOffsetLow, spawnOffsetHigh), dropHeight+i, Random.Range(spawnOffsetLow, spawnOffsetHigh)), Quaternion.identity);
+			i++;
+		}
+
+
+	}
+
+}
