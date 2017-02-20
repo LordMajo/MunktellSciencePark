f0908bb6fcc8d4b7081188d101554ba5ac50f0c1
Lord Majo <majo@tii.se>
2017-02-20 10:07:54 +0100

    lama
    tralalal

---
+++ Assets/CollisionSound.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class CollisionSound : MonoBehaviour {
+
+	public AudioClip boing;
+	private AudioSource source;
+
+	void Awake() {
+
+		source = GetComponent<AudioSource>();
+
+	}
+
+
+	void OnCollisionEnter(Collision coll) {
+	
+		source.PlayOneShot (boing, 1f);
+
+	}
+
+}
---
+++ Assets/HoverVolume.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HoverVolume : MonoBehaviour {
+
+	public float hoverForce = 12f;
+
+
+
+	void OnTriggerEnter(Collider other) {
+	
+		Debug.Log ("Collision enter");
+
+	}
+
+
+	void OnTriggerExit(Collider other) {
+		
+		Debug.Log ("Collision exit");
+
+	}
+
+
+	void OnTriggerStay(Collider other) {
+		
+		Debug.Log ("Collision stay");
+		other.GetComponent<Rigidbody>().AddForce (Vector3.up * hoverForce, ForceMode.Force);
+
+	}
+
+}
